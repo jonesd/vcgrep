6816c4edf2d49a526e51eaf42230569666d9ec35
David Jones <david@dgjones.info>
2015-02-14 00:06:41 -0500

    Add unit tests for matches

---
+++ test/matches.test.js
@@ -0,0 +1,62 @@
+"use strict";
+
+var should = require('should');
+
+var Matches = require('../lib/matches');
+
+describe('Matches', function() {
+  var matches;
+  beforeEach(function() {
+    matches = new Matches();
+  });
+
+  describe('init', function() {
+    it('should be initialized to zero and empty', function() {
+      matches.max.should.be.eql(0);
+      matches.total.should.be.eql(0);
+      matches.keys.should.be.empty;
+    });
+  });
+
+  describe('increment', function() {
+    it('should add if unknown key', function() {
+      matches.increment('testKey');
+      matches.keys['testKey'].should.eql(1);
+    });
+    it('should increment value for known key', function() {
+      matches.increment('testKey');
+      matches.increment('testKey');
+      matches.keys['testKey'].should.eql(2);
+    });
+    it('should keep total of all increments', function() {
+      matches.increment('key0');
+      matches.increment('key0');
+      matches.increment('key1');
+      matches.total.should.eql(3);
+    });
+    it('should remember maximum value of any key', function() {
+      matches.increment('key0');
+      matches.increment('key0');
+      matches.increment('key1');
+      matches.max.should.eql(2);
+    });
+  });
+
+  describe('values', function() {
+    it('should be empty initially', function() {
+      matches.values().should.be.empty;
+    });
+    it('should provide pairs of keys and values sorted largest first', function() {
+        matches.increment('key0');
+        matches.increment('key1');
+        matches.increment('key1');
+        matches.values().should.eql([['key1', 2], ['key0', 1]]);
+    });
+    it('should limit to first n values', function() {
+      matches.increment('key0');
+      matches.increment('key1');
+      matches.increment('key1');
+      matches.values(1).should.eql([['key1', 2]]);
+    });
+  });
+});
