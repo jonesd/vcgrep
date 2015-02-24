d8e8368be3aef6876cbe32e08784f3f41cc0ea04
David Jones <david@dgjones.info>
2015-02-23 22:03:01 -0500

    pass options to search as object rather than multiple params

--- examples/search.js
+++ examples/search.js
@@ -4,7 +4,12 @@
 
 var vcgrep = require('vcgrep');
 
-vcgrep.searchFiles(/(\w+)+/g, ['README.md'], false, 'none', function(err, matches) {
+var options = {
+  progress: 'none',
+
+};
+
+vcgrep.searchFiles(/(\w+)+/g, ['README.md'], options, function(err, matches) {
   vcgrep.writeOutput('histogram', matches);
   console.log('Found '+matches.total+' hits across: '+matches.totalFiles+' files');
 });
